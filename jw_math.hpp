dbe120d060a6fd1cd6e1f70d6a2c320e6e46d31e
jonathanpwalton <107540254+jonathanpwalton@users.noreply.github.com>
2024-04-30 15:03:40 +0900

    Fixed formatting

--- jw_math.hpp
+++ jw_math.hpp
@@ -54,82 +54,99 @@ namespace jw
     vec2(f32 s) : x(s), y(s) {}
     vec2(f32 x, f32 y) : x(x), y(y) {}
 
-    f32 dot(const vec2& b) const {
+    f32 dot(const vec2 &b) const
+    {
       return x * b.x + y * b.y;
     }
 
-    f32 length_squared() const {
+    f32 length_squared() const
+    {
       return dot(*this);
     }
 
-    f32 length() const {
+    f32 length() const
+    {
       return sqrtf(length_squared());
     }
 
-    vec2& normalize() {
+    vec2 &normalize()
+    {
       f32 l = length();
       x /= l;
       y /= l;
       return *this;
     }
 
-    vec2 normalized() {
+    vec2 normalized()
+    {
       return vec2(*this).normalize();
     }
 
-    vec2 operator+(f32 s) const {
+    vec2 operator+(f32 s) const
+    {
       return vec2(x + s, y + s);
     }
 
-    vec2 operator-(f32 s) const {
+    vec2 operator-(f32 s) const
+    {
       return vec2(x - s, y - s);
     }
 
-    vec2 operator*(f32 s) const {
+    vec2 operator*(f32 s) const
+    {
       return vec2(x * s, y * s);
     }
 
-    vec2 operator/(f32 s) const {
+    vec2 operator/(f32 s) const
+    {
       return vec2(x / s, y / s);
     }
 
-    vec2 operator+(const vec2& b) const {
+    vec2 operator+(const vec2 &b) const
+    {
       return vec2(x + b.x, y + b.y);
     }
-    
-    vec2 operator-(const vec2& b) {
+
+    vec2 operator-(const vec2 &b)
+    {
       return vec2(x - b.x, y - b.y);
     }
 
-    vec2 &operator+=(f32 s) {
+    vec2 &operator+=(f32 s)
+    {
       x += s;
       y += s;
       return *this;
     }
 
-    vec2 &operator-=(f32 s) {
+    vec2 &operator-=(f32 s)
+    {
       x -= s;
       y -= s;
       return *this;
     }
 
-    vec2 &operator*=(f32 s) {
+    vec2 &operator*=(f32 s)
+    {
       x *= s;
       y *= s;
       return *this;
     }
 
-    vec2 &operator/=(f32 s) {
+    vec2 &operator/=(f32 s)
+    {
       x /= s;
       y /= s;
       return *this;
     }
 
-    vec2 &operator+=(const vec2& b) {
+    vec2 &operator+=(const vec2 &b)
+    {
       return *this = *this + b;
     }
-    
-    vec2 &operator-=(const vec2& b) {
+
+    vec2 &operator-=(const vec2 &b)
+    {
       return *this = *this - b;
     }
   };
@@ -141,19 +158,23 @@ namespace jw
     vec3(f32 x, f32 y, f32 z) : x(x), y(y), z(z) {}
     vec3(vec2 v, f32 z = 0.0F) : x(v.x), y(v.y), z(z) {}
 
-    f32 dot(const vec3& b) const {
+    f32 dot(const vec3 &b) const
+    {
       return x * b.x + y * b.y + z * b.z;
     }
 
-    f32 length_squared() const {
+    f32 length_squared() const
+    {
       return dot(*this);
     }
 
-    f32 length() const {
+    f32 length() const
+    {
       return sqrtf(length_squared());
     }
 
-    vec3& normalize() {
+    vec3 &normalize()
+    {
       f32 l = length();
       x /= l;
       y /= l;
@@ -161,71 +182,85 @@ namespace jw
       return *this;
     }
 
-    vec3 normalized() {
+    vec3 normalized()
+    {
       return vec3(*this).normalize();
     }
 
-    vec3 cross(const vec3& b) {
+    vec3 cross(const vec3 &b)
+    {
       return vec3(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x);
     }
 
-    vec3 operator+(f32 s) const {
+    vec3 operator+(f32 s) const
+    {
       return vec3(x + s, y + s, z + s);
     }
 
-    vec3 operator-(f32 s) const {
+    vec3 operator-(f32 s) const
+    {
       return vec3(x - s, y - s, z - s);
     }
 
-    vec3 operator*(f32 s) const {
+    vec3 operator*(f32 s) const
+    {
       return vec3(x * s, y * s, z * s);
     }
 
-    vec3 operator/(f32 s) const {
+    vec3 operator/(f32 s) const
+    {
       return vec3(x / s, y / s, z / s);
     }
 
-    vec3 operator+(const vec3& b) const {
+    vec3 operator+(const vec3 &b) const
+    {
       return vec3(x + b.x, y + b.y, z + b.z);
     }
-    
-    vec3 operator-(const vec3& b) {
+
+    vec3 operator-(const vec3 &b)
+    {
       return vec3(x - b.x, y - b.y, y - b.y);
     }
 
-    vec3 &operator+=(f32 s) {
+    vec3 &operator+=(f32 s)
+    {
       x += s;
       y += s;
       z += s;
       return *this;
     }
 
-    vec3 &operator-=(f32 s) {
+    vec3 &operator-=(f32 s)
+    {
       x -= s;
       y -= s;
-      z-= s;
+      z -= s;
       return *this;
     }
 
-    vec3 &operator*=(f32 s) {
+    vec3 &operator*=(f32 s)
+    {
       x *= s;
       y *= s;
       z *= s;
       return *this;
     }
 
-    vec3 &operator/=(f32 s) {
+    vec3 &operator/=(f32 s)
+    {
       x /= s;
       y /= s;
       z /= s;
       return *this;
     }
 
-    vec3 &operator+=(const vec3& b) {
+    vec3 &operator+=(const vec3 &b)
+    {
       return *this = *this + b;
     }
-    
-    vec3 &operator-=(const vec3& b) {
+
+    vec3 &operator-=(const vec3 &b)
+    {
       return *this = *this - b;
     }
   };
@@ -237,19 +272,23 @@ namespace jw
     vec4(f32 x, f32 y, f32 z, f32 w) : x(x), y(y), z(z), w(w) {}
     vec4(vec3 v, f32 w = 0.0F) : x(v.x), y(v.y), z(v.z), w(w) {}
 
-    f32 dot(const vec4& b) const {
+    f32 dot(const vec4 &b) const
+    {
       return x * b.x + y * b.y + z * b.z + w * b.w;
     }
 
-    f32 length_squared() const {
+    f32 length_squared() const
+    {
       return dot(*this);
     }
 
-    f32 length() const {
+    f32 length() const
+    {
       return sqrtf(length_squared());
     }
 
-    vec4& normalize() {
+    vec4 &normalize()
+    {
       f32 l = length();
       x /= l;
       y /= l;
@@ -258,35 +297,43 @@ namespace jw
       return *this;
     }
 
-    vec4 normalized() {
+    vec4 normalized()
+    {
       return vec4(*this).normalize();
     }
 
-    vec4 operator+(f32 s) const {
+    vec4 operator+(f32 s) const
+    {
       return vec4(x + s, y + s, z + s, w + s);
     }
 
-    vec4 operator-(f32 s) const {
+    vec4 operator-(f32 s) const
+    {
       return vec4(x - s, y - s, z - s, w - s);
     }
 
-    vec4 operator*(f32 s) const {
+    vec4 operator*(f32 s) const
+    {
       return vec4(x * s, y * s, z * s, w * s);
     }
 
-    vec4 operator/(f32 s) const {
+    vec4 operator/(f32 s) const
+    {
       return vec4(x / s, y / s, z / s, w / s);
     }
 
-    vec4 operator+(const vec4& b) const {
+    vec4 operator+(const vec4 &b) const
+    {
       return vec4(x + b.x, y + b.y, z + b.z, w + b.w);
     }
-    
-    vec4 operator-(const vec4& b) {
+
+    vec4 operator-(const vec4 &b)
+    {
       return vec4(x - b.x, y - b.y, y - b.y, w - b.w);
     }
 
-    vec4 &operator+=(f32 s) {
+    vec4 &operator+=(f32 s)
+    {
       x += s;
       y += s;
       z += s;
@@ -294,7 +341,8 @@ namespace jw
       return *this;
     }
 
-    vec4 &operator-=(f32 s) {
+    vec4 &operator-=(f32 s)
+    {
       x -= s;
       y -= s;
       z -= s;
@@ -302,7 +350,8 @@ namespace jw
       return *this;
     }
 
-    vec4 &operator*=(f32 s) {
+    vec4 &operator*=(f32 s)
+    {
       x *= s;
       y *= s;
       z *= s;
@@ -310,7 +359,8 @@ namespace jw
       return *this;
     }
 
-    vec4 &operator/=(f32 s) {
+    vec4 &operator/=(f32 s)
+    {
       x /= s;
       y /= s;
       z /= s;
@@ -318,11 +368,13 @@ namespace jw
       return *this;
     }
 
-    vec4 &operator+=(const vec4& b) {
+    vec4 &operator+=(const vec4 &b)
+    {
       return *this = *this + b;
     }
-    
-    vec4 &operator-=(const vec4& b) {
+
+    vec4 &operator-=(const vec4 &b)
+    {
       return *this = *this - b;
     }
   };
@@ -461,15 +513,14 @@ namespace jw
 
       return r;
     }
-    
-    vec4 operator*(const vec4& b)
+
+    vec4 operator*(const vec4 &b)
     {
       return vec4(
-        m00 * b.x + m10 * b.y + m20 * b.z + m30 * b.w,
-        m01 * b.x + m11 * b.y + m21 * b.y + m31 * b.w,
-        m02 * b.x + m12 * b.y + m22 * b.z + m32 * b.w,
-        m03 * b.x + m13 * b.y + m23 * b.z + m33 * b.w
-      );
+          m00 * b.x + m10 * b.y + m20 * b.z + m30 * b.w,
+          m01 * b.x + m11 * b.y + m21 * b.y + m31 * b.w,
+          m02 * b.x + m12 * b.y + m22 * b.z + m32 * b.w,
+          m03 * b.x + m13 * b.y + m23 * b.z + m33 * b.w);
     }
 
     mat4 &operator*=(const mat4 &b)
